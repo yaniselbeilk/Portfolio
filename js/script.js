c72ebd912f48492c66adb0b2d7e179f32be3502f
Yanis EL BEILK <yanis.lblk@gmail.com>
2023-01-27 15:12:36 +0100

    correction of a bug of the navigation page in mobile format

--- js/script.js
+++ js/script.js
@@ -2,4 +2,11 @@ const menuButton = document.getElementById("menu-button");
 const menu = document.getElementById("nav-links");
 menuButton.addEventListener('click',()=>{
     menu.classList.toggle('mobile-menu');
+    menu.style.transition = "all 0.5s ease";
+})
+addEventListener('resize', ()=>{
+    if (window.screen.width > 900) {
+        menu.classList.toggle('mobile-menu', false);
+        menu.style.removeProperty('transition');
+    }
 })
