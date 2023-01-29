be226eaacc9867ce59a8b1aad53271136521679c
Yanis EL BEILK <yanis.lblk@gmail.com>
2023-01-29 23:09:07 +0100

    Change the structure of javascript files

--- js/header.js
+++ js/header.js
@@ -1,5 +1,9 @@
 const menuButton = document.getElementById("menu-button");
 const menu = document.getElementById("nav-links");
+
+particlesJS.load('particles-js', 'assets/particles.json', function () {
+    console.log('callback - particles.js config loaded');
+})
 menuButton.addEventListener('click',()=>{
     menu.classList.toggle('mobile-menu');
     menu.style.transition = "all 0.5s ease";
