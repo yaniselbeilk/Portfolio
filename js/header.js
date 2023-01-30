bf86e79111dd47507744886b5bd34fcea1b1349b
Yanis EL BEILK <yanis.lblk@gmail.com>
2023-01-30 20:41:04 +0100

    correction of the load of the particles

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
 const menuButton = document.getElementById("menu-button");
 const menu = document.getElementById("nav-links");
 
-particlesJS.load('particles-js', 'assets/particles.json', function () {
+particlesJS.load('particles', 'assets/particles.json', function () {
     console.log('callback - particles.js config loaded');
 })
 menuButton.addEventListener('click',()=>{
