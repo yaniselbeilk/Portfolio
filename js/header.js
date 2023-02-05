952556540f2308e8c0fc787cc97fcd992ca28ca9
Yanis EL BEILK <yanis.lblk@gmail.com>
2023-02-05 14:10:21 +0100

    modification of coloration

--- js/header.js
+++ js/header.js
@@ -1,16 +1,21 @@
+particlesJS.load('particles', 'assets/particles.json', function () {
+    console.log('callback - particles.js config loaded');
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    
 const menuButton = document.getElementById("menu-button");
 const menu = document.getElementById("nav-links");
 
-particlesJS.load('particles', 'assets/particles.json', function () {
-    console.log('callback - particles.js config loaded');
-})
 menuButton.addEventListener('click',()=>{
     menu.classList.toggle('mobile-menu');
     menu.style.transition = "all 0.5s ease";
-})
-addEventListener('resize', ()=>{
+});
+
+window.addEventListener('resize', ()=>{
     if (window.screen.width > 900) {
         menu.classList.toggle('mobile-menu', false);
         menu.style.removeProperty('transition');
     }
-})
+});       
+});
