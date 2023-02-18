8e7a5977055535e5584886765fe00ed1db62dafc
Yanis EL BEILK <yanis.lblk@gmail.com>
2023-02-18 02:39:13 +0100

    improvement of the structure of the project, code and responsivity for the skills sections

--- js/skills.js
+++ js/skills.js
@@ -1,29 +1,37 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
     const titleSkillsCategory = document.getElementById('title-skills-category').getElementsByTagName('button');
-    const containerSkills = document.getElementsByClassName('container-skills');
-    const blueAzure = getComputedStyle(document.querySelector(':root')).getPropertyValue('--blue-azure');
+    const containerSkills = document.getElementsByClassName('container-skills')
 
 var indexButton = 0
-titleSkillsCategory[indexButton].classList.toggle('button-skills', true);
+
+if (window.screen.width >= 1000) {
+    titleSkillsCategory[indexButton].classList.toggle('button-skills', true);
+    containerSkills[indexButton].classList.toggle('skills-visible', true);
+}
+
 for (let index = 0; index < titleSkillsCategory.length; index++) {
     titleSkillsCategory[index].addEventListener('click', ()=>{
         titleSkillsCategory[index].classList.toggle('button-skills', true);
-        containerSkills[index].style.display = 'contents';
+        containerSkills[index].classList.toggle('skills-visible', true);
 
         if (indexButton!=index) {
             titleSkillsCategory[indexButton].classList.toggle('button-skills', false);
-            containerSkills[indexButton].style.display = 'none'; 
+            containerSkills[indexButton].classList.toggle('skills-visible', false);
             indexButton = index;  
         }
-        /*
-        containerSkills[index].style.display = 'contents';
-        titleSkillsCategory[index].style.backgroundColor = blueAzure;
-        
-        containerSkills[indexButton].style.display = 'none';
-        titleSkillsCategory[indexButton].style.backgroundColor = "White";
-        */
     });
 }
+
+window.addEventListener('resize', () => {
+    if (window.screen.width < 1000) {
+        for (let index = 0; index < containerSkills.length; index++) {
+            containerSkills[index].classList.toggle('skills-visible', false);   
+        }
+    } else {
+        titleSkillsCategory[indexButton].classList.toggle('button-skills', true);
+        containerSkills[indexButton].classList.toggle('skills-visible', true);
+    }
+});
 
 });
